921a621bea72f874216d5f88d5c0e7714ff490b3
shemar <nastavnyk@gmail.com>
2021-03-29 19:39:42 +0200

    added more asserts

---
+++ tests/test_preconditions.py
@@ -0,0 +1,21 @@
+from tests import app
+
+
+def test_add_and_restart_browser():
+    app.open()
+
+    app.add('a')
+    app.restart_page()
+
+    app.todos_should_be('a')
+    app.items_left_should_be(1)
+    app.footer_should_be_visible()
+    app.clear_completed_should_be_hidden()
+
+
+def test_open():
+
+    app.open()
+
+    app.todos_should_be()
+    app.footer_should_be_hidden()
